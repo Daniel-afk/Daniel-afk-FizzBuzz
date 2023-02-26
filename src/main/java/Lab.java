42cdedeebada46c5e3ef217c5670f36cbbc45f61
Daniel-afk <david.garcia0343@gmail.com>
2023-02-26 03:00:45 +0000

    fizz

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -20,7 +20,7 @@ public class Lab {
         } else if (n%5 == 0){
             return "Buzz";
         } else if (n%3 == 0) {
-            return "Fizz";
+            return   "Fizz";
         } else {
             return "";    
         }
